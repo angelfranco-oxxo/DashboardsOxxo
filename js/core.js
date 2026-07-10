e1b91ad2788da5c54bc02f72ba5ed9ca38a5ac7d
Angel De Jesus Franco Gijon <angel.franco@oxxo.com>
2026-07-10 08:12:56 -0600

    Actualizar URL de Apps Script admin

--- js/core.js
+++ js/core.js
@@ -19,7 +19,7 @@ const SHEETS_CONFIG = {
 
   // URL del Web App de Apps Script para publicar desde admin.html.
   // Cuando se configure una vez, el panel admin la usara automaticamente.
-  ADMIN_UPLOAD_URL: "https://script.google.com/macros/s/AKfycbzBSOgPa7zQI6ytmmLgKnJpsMH5XITgBMM1XlKba65plWf0863YufxM9R-GsHfjVcfA/exec",
+  ADMIN_UPLOAD_URL: "https://script.google.com/macros/s/AKfycbzK4UiB9JHq84EDNTVTwLGZiN39R9rI5L0sk-_p0TgR2ZISmgXSxsILyxjG_MZcROLq/exec",
 
   // Nombres exactos de cada pestaña en Google Sheets
   TABS: {
